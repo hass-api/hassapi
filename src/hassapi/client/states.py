979a0353fa089befd2cbe85ebe4f9b15d8ca622c
Renato <33936751+N4S4@users.noreply.github.com>
2023-03-09 15:06:48 +0100

    Update states.py
    from /Kuwertzel/hassapi-extended

--- src/hassapi/client/states.py
+++ src/hassapi/client/states.py
@@ -12,8 +12,10 @@ class StatesClient(BaseClient):
         """Get ``entity_id`` state."""
         return State(**self._get(f"states/{entity_id}"))  # type: ignore
 
-    def set_state(self, entity_id: str, state: HassValueType) -> State:
-        """Set ``entity_id`` state."""
+    def set_state(self, entity_id: str, state: HassValueType, attributes: dict = None) -> State:
+        """Set ``entity_id`` state and optionally attributes."""
+        if attributes:
+            return State(**self._post(f"states/{entity_id}", state=state, attributes=attributes))  # type: ignore
         return State(**self._post(f"states/{entity_id}", state=state))  # type: ignore
 
     def get_states(self) -> StateList:
