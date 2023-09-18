1f9982eb86436a6cdcd5b82e3724c3dde31cd31c
Vadim Titov <titov.hse@gmail.com>
2023-09-18 13:25:24 +0100

    Add pypi publish on release and CI improvements

--- setup.py
+++ setup.py
@@ -1,5 +1,36 @@
 """Handle module packaging."""
 
+import subprocess
+import textwrap
+
 from setuptools import setup
 
-setup()
+
+def _get_release_version() -> None:
+    """Get git release tag version."""
+    version = (
+        subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+        .stdout.decode("utf-8")
+        .strip()
+    )
+    print("VERSION: ", version)
+    return version
+
+
+def _write_version(version: str) -> None:
+    """Write version to version.py."""
+    with open("src/hassapi/version.py", "w") as file:
+        file.write(
+            textwrap.dedent(
+                f'''
+                """Host package version, generated on build."""
+                __version__ = "{version}"
+                '''
+            ).lstrip()
+        )
+
+
+version = _get_release_version()
+
+_write_version(version)
+setup(version=version)
